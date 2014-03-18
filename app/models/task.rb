91cdc24c03e9d4c80c49e08e9344fbc2e56c5b02
menslow <michael.enslow@gmail.com>
2014-03-18 16:46:51 -0700

    added custom validation for due and start date

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,17 +4,19 @@ class Task < ActiveRecord::Base
   validates :name, 
     presence: true, 
     length: { maximum: 500, too_long: "must have at most %{count} characters" }
-  validates :product_id, 
+  validates :project_id, 
     presence: true, 
     numericality: { only_integer: true }
   validates :user_id, 
+    allow_nil: true,
     numericality: { only_integer: true }
-  validates :due_must_make_sense
+  validate :due_must_make_sense
 
   private
     def due_must_make_sense
       if due < start
-        errors.add(:due, 'Due date can not be before start date')
+        errors.add(:due, 'Due date cannot be before start date')
       end
     end
+
 end
